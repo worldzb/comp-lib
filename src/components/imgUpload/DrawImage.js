6ea4153db84d8ea3293a92050f7f13ac1099bd11
worldzb <yang641367741@gmail.com>
2017-12-30 15:05:06 +0800

    前端上传部分完成，确定布局

--- src/components/imgUpload/DrawImage.js
+++ src/components/imgUpload/DrawImage.js
@@ -2,7 +2,7 @@
 * @Author: yang
 * @Date:   2017-12-27 15:15:24
 * @Last Modified by:   yang
-* @Last Modified time: 2017-12-30 09:34:18
+* @Last Modified time: 2017-12-30 13:13:40
 */
 
 class DrawImg{
@@ -14,16 +14,18 @@ class DrawImg{
 
 		this.beginX=0;//画图初始坐标X
 		this.beginY=0;//画图初始坐标X
-		this.imgWidth=0;
-		this.imgHeight=0;
+		this.imgWidth=0;//画图实际宽度
+		this.imgHeight=0;//画图实际高度
 		this.beginPoint=[0,0];//画图初始坐标点
 		this.multiple=0.05;
 
 		this.isDregStart=false;//是否执行拖拽
+		this.imgDataUrl='';//返回在canvas经过裁切的图像数据，以base64的方式
+		this.eventUpdate=null;//委托，绑定实例自定事件
 
 		this.Img=new Image();//需要绘制的图像
-		this.createCanvas();
-		this.addEvent();
+		this.createCanvas();//初始化canvas
+		this.addEvent();//事件绑定
 	}
 	/**
 	 * 创建canvas画板对象
@@ -35,7 +37,7 @@ class DrawImg{
 	}
 	//绑定事件
 	addEvent(){
-		var obj=this;
+		var obj=this;//异步调用时，this转换
 		var x=0;
 		var y=0;
 		var point=[0,0];
@@ -50,18 +52,19 @@ class DrawImg{
 		this.canvas.addEventListener('mousemove',(event)=>{
 			event = event || window.event; //兼容处理
 			if(obj.isDregStart){
-				console.log(x+","+y);
+				//console.log(x+","+y);
 				obj.beginX=event.clientX-x;
 				obj.beginY=event.clientY-y;
 				obj.ctx.clearRect(0, 0, obj.canvas.width,obj.canvas.height);
-				console.log(obj.imgWidth);
+				//console.log(obj.imgWidth);
 				obj.ctx.drawImage(obj.Img,obj.beginX,obj.beginY,obj.imgWidth,obj.imgHeight);
+				obj.imgDataUrl=this.canvas.toDataURL();
 			}
 		});
 		//鼠标弹起事件
 		this.canvas.addEventListener('mouseup',()=>{
 			obj.isDregStart=false;
-			obj.rtImageData()
+			obj.eventUpdate();//在鼠标弹起时，外部事件
 		});
 		//鼠标移出canvas
 		this.canvas.addEventListener('mouseout',()=>{
@@ -74,13 +77,14 @@ class DrawImg{
 				obj.imgWidth*=(1+this.multiple);
 				obj.imgHeight*=(1+this.multiple);
 				obj.ctx.drawImage(obj.Img,obj.beginX,obj.beginY,obj.imgWidth,obj.imgHeight);
-				console.log('放大');
+				//console.log('放大');
 			}else{
 				obj.imgWidth*=(1-this.multiple);
 				obj.imgHeight*=(1-this.multiple);
 				obj.ctx.drawImage(obj.Img,obj.beginX,obj.beginY,obj.imgWidth,obj.imgHeight);
-				console.log('缩小');
+				//console.log('缩小');
 			}
+			obj.eventUpdate();//图片大小发生变化时，触发外部事件
 		});
 	}
 	/**
@@ -93,7 +97,7 @@ class DrawImg{
 		fileReader.readAsDataURL(file);
 		var that=this;
 		fileReader.onloadend=()=>{
-			that.Img.src=fileReader.result;
+			that.Img.src=fileReader.result;//在图片加载完成后，将其数据以base64 赋值给Img对象
 		}
 	}
 	/**
@@ -117,6 +121,7 @@ class DrawImg{
 			this.imgHeight=this.Img.height;
 			this.ctx.clearRect(0, 0, this.canvas.width,this.canvas.height);
 			this.ctx.drawImage(this.Img,this.beginX,this.beginY,this.Img.width,this.Img.height);
+			this.eventUpdate();
 		}
 	}
 
--- src/components/imgUpload/imgUpload.vue
+++ src/components/imgUpload/imgUpload.vue
@@ -1,14 +1,26 @@
 <template>
 	<div id="imgUpload">
-		<input id="ipt-file" class="ipt-file" type="file" name="" @change="fileChange()" style="outline:none">
-		<canvas id="canvas-img" width='500' height="400" v-model="image" v-show="isFileSelect"></canvas>
-		<div id="preview" v-show="isFileSelect" >
+		<form id="fileUpload" accept-charset="utf-8" enctype="multipart/form-data">
+			<input id="ipt-file" class="ipt-file" type="file" name="" @change="fileChange()" style="outline:none">
+		</form>
+		<canvas id="canvas-img" width='501' height="400" v-model="image" v-show="isFileSelect">
+			fsda
+		</canvas>
+		<div id="preview" v-show="isFileSelect">
 			<img :src="image" width="100" height="100" style="background:#eee;">
 			<img :src="image" width="100" height="100" style="background:#eee;border-radius: 50px">
+			<img :src="image" width="50" height="50" style="background:#eee;">
 			<img :src="image" width="50" height="50" style="background:#eee;border-radius: 50px">
+		</div>
+		<div id="submitBox" v-show="isFileSelect">
+			<button class="btn btn-success btn-block btn-sm"  
+			title="图片将被裁切压缩" @click="uploadImg()">上传</button>
+			<button class="btn btn-default btn-block btn-sm" 
+			title="图片未被裁切压缩" @click="uploadImgPri()">上传原图</button>
 		</div>
 	</div>
 </template>
+
 
 <script>
 	import DrawImg from './DrawImage.js';
@@ -34,13 +46,29 @@
 		},
 		methods:{
 			fileChange:function(){
+				var that=this;
 				if(this.drawImg===''){
 					this.drawImg=new DrawImg('ipt-file','canvas-img');
 				}
 				this.drawImg.drawCenter();
-				this.image=this.drawImg.rtImageData();
+				this.drawImg.eventUpdate=()=>{
+					that.image=that.drawImg.rtImageData();//获取canvas数据
+				}
+				//this.image=this.drawImg.rtImageData();
 				this.isFileSelect=true;
 			},
+			uploadImg:function(){
+				//图片上传
+				var that=this;
+				this.$http.post(this.imgUrl,new FormData($('#fileUpload')[0]),{
+					emulateJSON: true,
+				}).then((res)=>{
+					console.log(res);
+				});
+			},
+			uploadImgPri:function(){
+				//原始图片上传
+			}
 		}
 	}
 
@@ -51,8 +79,10 @@
 		border:1px solid #eee;width:502px;
 	}
 	#canvas-img{
-		border-top: 2px solid #ccc;
-		background: #ccc;cursor: move;
+		border-top: 2px solid #bbb;
+		border-bottom: 2px solid #bbb;
+		background: #ccc;
+		cursor:move;
 	}
 	input[type=file]{
 		padding: 0;
@@ -61,13 +91,29 @@
 		width: 100%;
 		border:0;
 		outline: none;
-		box-shadow: 0
+		box-shadow: 0 0 1px #999;
 	}
 	#preview{
-		margin-top: 5px;
+		width: 80%;
+		background: #eee;
+		box-shadow: 0 0 1px #999;
+		margin:0;
+		margin-top: -5px;
+		padding: 5px;
+		float: left;
 	}
 	#preview img{
-		margin-left: 10px;
+		margin-left: 5px;
 		box-shadow: 0 0 10px #999;
+	}
+	#submitBox{
+		width: 20%;
+		height: 110px;
+		background: #eee;
+		box-shadow: 0 0 1px #999;
+		margin:0;
+		margin-top: -5px;
+		padding: 5px;
+		float: left;
 	}
 </style>
